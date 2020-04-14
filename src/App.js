e89837c8370bf5c32134c676eacc71b21be7e702
roxyoanes <roxioanes11@gmail.com>
2020-04-14 11:08:51 +0200

    Add toggle between components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import RecipeList from "./RecipeList.js";
+import Search from "./Search"
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [toggle, setToggle] = React.useState(false);
+
+const toggleComponents = () => {
+    setToggle(!toggle)
+} 
   return (
     <div className="App">
         <p>
           hello
         </p>
+        <button onClick={toggleComponents}>ok</button>
+        {toggle ? <RecipeList /> : <Search />}
     </div>
   );
 }
---
+++ src/RecipeList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RecipeList = () => {
+  return(
+  <div>
+    <p>hey</p>
+  </div>
+  )
+  
+}
+
+export default RecipeList;
