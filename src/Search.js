8817d2369f871213076dfa10de3bb3d701f0b7c0
roxyoanes <roxioanes11@gmail.com>
2020-04-15 12:01:44 +0200

    add onpress input functionality

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-const Search = ({handleSearch, handleRecipeName, recipeName, searchRecipe}) => {
+const Search = ({handleSearch, handleRecipeName, handleIngredients, recipeName, ingredients, keyPress}) => {
 
   return(
   <div>
     <p>salut</p>
     <div className="search-container">
       <input type="text" onChange={handleRecipeName} value={recipeName} />
-      <input type="text" />
-      <button className="search-btn" onClick={searchRecipe}>Search</button>
+      <input type="text" onChange={handleIngredients} onKeyDown={keyPress} value={ingredients} />
+      <div>
+        <p>{ingredients}</p>
+      </div>
+      <button className="search-btn" onClick={handleSearch}>Search</button>
     </div>
   </div>
   )
