18d7c6f95e5dd37f215e3d5ff137479d1c5473b7
roxyoanes <roxioanes11@gmail.com>
2020-04-16 10:18:23 +0200

    Delete ingredient

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import RecipeList from "./RecipeList.js";
 import Search from "./Search";
 import { recipeApi } from "./api";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   const [toggle, setToggle] = React.useState(false);
@@ -13,64 +13,69 @@ const App = () => {
   const [error, setError] = React.useState(null);
 
   const handleSearch = () => {
-    if(recipeName && inputValue){
-      recipeApi(recipeName, inputValue).then(data => {
-        setRecipeName(data)
-        setInputValue(data)
-      })
-    } else{
+    if (recipeName && inputValue) {
+      recipeApi(recipeName, inputValue).then((data) => {
+        setRecipeName(data);
+        setInputValue(data);
+      });
+    } else {
       setRecipeName([]);
       setInputValue([]);
     }
-  }
+  };
 
   const handleRecipeName = (e) => {
-    setRecipeName(e.target.value)
-  }
+    setRecipeName(e.target.value);
+  };
 
   const handleInput = (e) => {
-    setInputValue(e.target.value)
-  }  
+    setInputValue(e.target.value);
+  };
 
   const toggleComponents = () => {
-      setToggle(!toggle)
-  }
+    setToggle(!toggle);
+  };
 
   const keyPress = (e) => {
-    if(e.keyCode === 13){
-      setIngredients([...ingredients, e.target.value])
-       console.log('value', e.target.value);
+    if (e.keyCode === 13) {
+      setIngredients([...ingredients, e.target.value]);
+      console.log("value", e.target.value);
     }
-  }
+  };
 
-  if(error){
-    return <p>{error}</p>
+  const deleteIngredient = (ingredient) => {
+    const newArray = ingredients.filter((element) => element !== ingredient);
+    setIngredients(newArray);
+  };
+
+  if (error) {
+    return <p>{error}</p>;
   }
-    return (
-      <div className="App">
-          <p>
-            Search recipe
-          </p>
-          <div className="btn-container">
-          <button className="toggle-btn" onClick={toggleComponents}>switch</button>
-          {toggle ? <RecipeList /> : 
-            <Search
-              recipeName={recipeName}
-              inputValue={inputValue}
-              handleSearch={handleSearch}
-              handleRecipeName={handleRecipeName}
-              handleInput={handleInput}
-              keyPress={keyPress}
-              setIngredients={setIngredients}
-              ingredients={ingredients}
-            />
-          }
-          </div>
-          
+  return (
+    <div className="App">
+      <p>Search recipe</p>
+      <div className="btn-container">
+        <button className="toggle-btn" onClick={toggleComponents}>
+          switch
+        </button>
+        {toggle ? (
+          <RecipeList />
+        ) : (
+          <Search
+            recipeName={recipeName}
+            inputValue={inputValue}
+            handleSearch={handleSearch}
+            handleRecipeName={handleRecipeName}
+            handleInput={handleInput}
+            keyPress={keyPress}
+            setIngredients={setIngredients}
+            ingredients={ingredients}
+            deleteIngredient={deleteIngredient}
+          />
+        )}
       </div>
-    );
-  
-}
-
+    </div>
+  );
+};
 
 export default App;
--- src/Search.js
+++ src/Search.js
@@ -1,23 +1,43 @@
 import React from "react";
 
-const Search = ({handleSearch, handleRecipeName, handleInput, recipeName, inputValue, keyPress, setIngredients, ingredients}) => {
-
-  return(
-  <div>
-    <p>salut</p>
-    <div className="search-container">
-      <input type="text" onChange={handleRecipeName} value={recipeName} />
-      <input type="text" onChange={handleInput} onKeyDown={keyPress} value={inputValue} />
-      <div>
-        {ingredients.map((ingredient) =>(
-          <li key={ingredient}>{ingredient}</li>
-        ))}
+const Search = ({
+  handleSearch,
+  handleRecipeName,
+  handleInput,
+  recipeName,
+  inputValue,
+  keyPress,
+  setIngredients,
+  ingredients,
+  deleteIngredient,
+}) => {
+  return (
+    <div>
+      <p>salut</p>
+      <div className="search-container">
+        <input type="text" onChange={handleRecipeName} value={recipeName} />
+        <input
+          type="text"
+          onChange={handleInput}
+          onKeyDown={keyPress}
+          value={inputValue}
+        />
+        <div>
+          {ingredients.map((ingredient) => (
+            <div>
+              <li key={ingredient}>{ingredient}</li>
+              <button onClick={() => deleteIngredient(ingredient)}>
+                Delete ingredient
+              </button>
+            </div>
+          ))}
+        </div>
+        <button className="search-btn" onClick={handleSearch}>
+          Search
+        </button>
       </div>
-      <button className="search-btn" onClick={handleSearch}>Search</button>
     </div>
-  </div>
-  )
-  
-}
+  );
+};
 
 export default Search;
